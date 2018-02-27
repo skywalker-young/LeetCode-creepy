395a9fcdd07b3838e801aea2fab40291dda32c83
膜法师 <30488918+skywalker-young@users.noreply.github.com>
2018-02-27 15:01:40 +0800

    Update selfdividing.py

--- selfdividing.py
+++ selfdividing.py
@@ -5,3 +5,7 @@
         :rtype: List[int]
         """
         return [num for num in range(left, right+1) if all((int(d) and not num % int(d)) for d in str(num))]
+
+    
+        def selfDividingNumbers(self, left, right):
+        return [num for num in range(left, right + 1) if not any([digit == '0' or num % int(digit) != 0 for digit in str(num)])]
