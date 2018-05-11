b4c001c491eb0b8a97e37629b01488a3a5de001e
膜法师 <30488918+skywalker-young@users.noreply.github.com>
2018-05-11 15:16:18 +0800

    matplotlib创建子图

---
+++ NumpyPandas/subplot.py
@@ -0,0 +1,25 @@
+
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+
+
+
+
+a=np.linspace(0,100,100)
+b=np.random.normal(0,1,100)
+ax=plt.subplot(2,1,1)  #这个图画在第一行
+
+ax.plot(a,label='a')
+plt.subplots_adjust(top=None,bottom=None)
+ax2=plt.subplot(2,1,2)  #这个图画在第二行
+plt.plot(b,label='b')
+
+plt.subplots_adjust(   wspace=None, hspace=0)
+plt.grid(True)
+plt.legend()
+plt.show()
